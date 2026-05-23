8849ad2d5512b77c4975b61b4ec2f8c2d95a4628
Az1myut <111507385+Az1myut@users.noreply.github.com>
2026-05-23 22:05:22 +0200

    Fix bug in insertion_sort implementation

--- insertion_sort_bug.py
+++ insertion_sort_bug.py
@@ -2,19 +2,17 @@ def insertion_sort(arr):
     # Traverse from the second element
     for i in range(1, len(arr)):
         key = arr[i]
-        j = i + 1
+        j = i - 1  # Correct index for insertion sort
 
-        
-        # one position ahead
-        while j > 0 and arr[j] > key:
+        # Move elements of arr[0..i-1], that are greater than key,
+        # to one position ahead of their current position
+        while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
 
-       
         arr[j + 1] = key
 
     return arr
-
 
 # Example usage
 numbers = [5, 2, 9, 1, 5, 6]
