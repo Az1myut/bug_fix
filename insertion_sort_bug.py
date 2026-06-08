d32764d15a41d429cc2dba174be80e325194113b
azamat <honeycomb0404@gmal.com>
2026-06-09 01:06:18 +0200

    BUg detected

--- insertion_sort_bug.py
+++ insertion_sort_bug.py
@@ -2,11 +2,11 @@ def insertion_sort(arr):
     # Traverse from the second element
     for i in range(1, len(arr)):
         key = arr[i]
-        j = i - 1  # index of element before key
+        j = i + 1  # index of element before key
 
         # Move elements of arr[0..i-1], that are greater than key,
         # to one position ahead of their current position
-        while j >= 0 and arr[j] > key:
+        while j >= 0 and arr[j] == key:
             arr[j + 1] = arr[j]
             j -= 1
 
