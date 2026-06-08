73285c471dd23edafe6591138c55313cd59b9329
azamat <honeycomb0404@gmal.com>
2026-06-09 00:52:21 +0200

    BUg detected

--- insertion_sort_bug.py
+++ insertion_sort_bug.py
@@ -6,9 +6,9 @@ def insertion_sort(arr):
 
         # Move elements of arr[0..i-1], that are greater than key,
         # to one position ahead of their current position
-        while j >= 0 and arr[j] > key:
+        while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
-            j -= 1
+            j += 1
 
         arr[j + 1] = key
 
