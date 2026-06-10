c7c3ffcb556a1eaa57957c6a8d2b44f36781baeb
azamat <honeycomb0404@gmal.com>
2026-06-11 00:10:37 +0200

    New bug

--- insertion_sort_bug.py
+++ insertion_sort_bug.py
@@ -1,20 +1,17 @@
 def insertion_sort(arr):
-    # Traverse from the second element
     for i in range(1, len(arr)):
         key = arr[i]
-        j = i - 1  # index of element before key
+        j = i  + 1  
 
-        # Move elements of arr[0..i-1], that are greater than key,
-        # to one position ahead of their current position
+ 
         while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
+            arr[j - 1] = arr[j]
+            j += 1
 
         arr[j + 1] = key
 
     return arr
 
-# Example usage
 numbers = [5, 2, 9, 1, 5, 6]
 
 sorted_numbers = insertion_sort(numbers)
