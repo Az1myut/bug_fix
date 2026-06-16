4be997ab07be40638c2f8e0a7d06b490fa0d9910
Az1myut <111507385+Az1myut@users.noreply.github.com>
2026-06-16 14:20:42 +0200

    Fix insertion sort bug: correct the initial value of j from i+1 to i-1

--- insertion_sort_bug.py
+++ insertion_sort_bug.py
@@ -4,7 +4,7 @@ def insertion_sort(arr):
     
     for i in range(1, len(arr)):
         key = arr[i]
-        j = i + 1
+        j = i - 1
 
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
